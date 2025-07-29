15391054c58700f395d58f8d20fedc061fe203f8
6Neel9 <nilmandal300@gmail.com>
2025-07-29 16:43:39 +0530

    update url

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -24,7 +24,7 @@ const Login = ({ setIsAuthenticated }) => {
     setError('');
 
     try {
-      const res = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}auth/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form)
